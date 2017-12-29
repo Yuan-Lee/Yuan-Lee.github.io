4047ad9139c9e113d1325e386aa684af853f6c53
yuan <liyuan6666@yeah.net>
2017-12-29 17:26:16 +0800

    add router

---
+++ router/router.js
@@ -0,0 +1,70 @@
+
+(function(){
+	"use strict";
+
+	var EventUtil = {
+		on: function(element, type, callback){
+			if(element.addEventListener){
+				element.addEventListener(type, callback, false);
+			}else if(element.attachEvent){
+				element.attachEvent('on' + type, callback);
+			}else{
+				element['on' + type] = callback;
+			}
+		},
+		off: function(element, type, callback){
+			if(element.removeEventListener){
+				element.removeEventListener(type, callback, false);
+			}else if(element.detachEvent){
+				element.detachEvent('on' + type, callback);
+			}else{
+				element['on' + type] = null;
+			}
+		}
+	};
+
+	var Router = function(routes){
+		this.routes = routes;
+		this.curHash = window.location.hash;
+	};
+
+	Router.prototype.addRoute = function(path, callback){
+		this.routes[path] = callback;
+	}
+
+	Router.prototype.route = function(path, callback){
+		if(typeof this.routes[path] !== 'function'){
+			this.addRoute(path, callback);
+			location.hash = path
+		}else{
+			this.routes[path]();
+		}
+	};
+
+	Router.prototype.reload = function(path){
+		var h = window.location.hash.split('#')[1];
+		if(h === '') return;
+		this.routes[h]();
+	};
+
+	Router.prototype.checkHash = function(){
+		var h = window.location.hash;
+		if(h != this.curHash){
+			this.curHash = h;
+			this.reload();
+		}
+	};
+
+	Router.prototype.init = function(){
+		if('onpopstate' in window){
+			EventUtil.on(window, 'popstate', this.reload, false);
+		}
+		EventUtil.on(window, 'hashchange', this.reload, false);
+		
+		EventUtil.on(window, 'load', this.reload, false);
+	};
+
+	window.Router = Router;
+})();
+
+	
